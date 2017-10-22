59615063f0704ae123cec435f44882a9896b1dab
DawidPi <pilarski.dawid94@gmail.com>
2017-10-22 16:20:34 +0200

    clearing all bits and setting all bits implemented

--- register_access.hpp
+++ register_access.hpp
@@ -8,6 +8,7 @@
 #include <type_traits>
 #include <utility>
 #include <cstdint>
+#include <limits>
 
 namespace register_access {
 
@@ -55,23 +56,45 @@ namespace register_access {
                 is_ptr_size,
                 std::uint8_t, std::uint16_t, std::uint16_t, std::uint64_t>::type;
 
-        template <typename ...Args>
-        constexpr  sumBits(){
+        template <typename T1, typename T2, typename ...T>
+        struct are_same{
+            static constexpr bool value = std::is_same<T1, T2>::value and are_same<T...>::value;
+        };
 
-        }
+        template <typename T1, typename T2>
+        struct are_same<T1, T2>{
+            static constexpr bool value = std::is_same<T1, T2>::value;
+        };
 
-        constexpr sumBits(){
+        template <typename T1, typename... T>
+        struct first_type{ // todo change for list_idx, which gets nth idx of the given parameter pack
+            using type = T1;
+        };
 
-        }
+        template <typename... T>
+        struct common_type{
+            static_assert(are_same<T...>::value, "Given types are not the same. Cannot determine common type");
+            using type = typename first_type<T...>::type;
+        };
+
     }
 
-    template<utils::pointer_int_type address>
+    template<utils::pointer_int_type address, typename underlying_type>
     struct register_access{
-        template <typename ...Args>
-        void setBits(Args...){
+        static constexpr underlying_type bitsClear = 0;
+        static constexpr underlying_type bitsSet = std::numeric_limits<underlying_type>::max();
 
+
+
+        void clear(){
+            *reinterpret_cast<volatile underlying_type*>(address) = bitsClear;
+        }
+
+        void setAll(){
+            *reinterpret_cast<volatile underlying_type*>(address) = bitsSet;
         }
     };
+
 }
 
 #endif //REGISTERACCESS_REGISTERACCESS_HPP
--- test.cpp
+++ test.cpp
@@ -17,7 +17,7 @@ namespace test{
 
 int main(){
     using namespace register_access;
-    using namespace register_access::utils;
+    using namespace utils;
     using namespace test;
 
     static_assert(are_same_size<int, int>::value, "are_same_size not implemented correctly");
@@ -30,11 +30,17 @@ int main(){
     static_assert(eq<select_if<check_if_is_void, int, char>::type, select_if_not_selected>, "select_if not implemented correctly");
     static_assert(eq<select_if<check_if_is_void, int, char, void>::type, void>, "select_if not implemented correctly");
 
-    static_assert(eq<pointer_int_type, uint32_t> || eq<pointer_int_type, uint64_t >,
+    static_assert(eq<pointer_int_type, uint32_t> || eq<pointer_int_type, uint64_t>,
                     "pointer_int_type is not implemented correctly or very strange architecture is chosen");
     static_assert(!eq<pointer_int_type, uint8_t>,
                     "pointer_int_type is not implemented correctly or very strange architecture is chosen");
 
+    static_assert(are_same<void, void>::value, "are_same not implemented correctly");
+    static_assert(!are_same<testA, testB>::value, "are_same not implemented correctly");
+    static_assert(are_same<void, void, void ,void>::value, "are_same not implemented correctly");
+    static_assert(!are_same<void, int, void ,void>::value, "are_same not implemented correctly");
+    static_assert(!are_same<testA, int, testB,void>::value, "are_same not implemented correctly");
+    static_assert(are_same<testA, testA, testA, testA>::value, "are_same not implemented correctly");
 
 
 }
